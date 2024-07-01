dc137e2c6c74cf22d1d0a15a1314252bc149447b
pratima-parajuli <61457287+pratima-parajuli@users.noreply.github.com>
2024-07-01 22:29:25 +0000

    conversionofdatatype

---
+++ 01_basic/datatypes-conversion.js
@@ -0,0 +1,15 @@
+let score = "33bv"
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(typeof score)
+// console.log(valueInNumber)
+//NaN (not a number) is type of varaible when we conver number NaN 
+//"33aba"=> might give NaN 
+//true = > 1, false => 0
+//""=> false 
+//"ahvhah"=> true
+
+let name ="abav"
+let valueInName = Boolean(name)
+console.log(valueInName)
