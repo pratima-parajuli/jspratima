842c40119b062982e0891ab4c49c7a22afa1eedd
pratima-parajuli <61457287+pratima-parajuli@users.noreply.github.com>
2024-07-01 22:09:49 +0000

    data-types

---
+++ 01_basic/01-datatypes-ECMA.js
@@ -0,0 +1,18 @@
+ "use strict" //treat all JS code as new version
+ alert ("hello") // we cannot this format for alert and we are using nodejs not browser
+ console.log(3
+    +3
+ ) // this is not a good provide and avoid doing this and try maintaing readbility
+
+//  ECMA is javascript standards
+
+let name = "hekko" // this is string
+let age= 10  // numeric
+
+let isLoggedIn = False // this is boolean
+
+//null this is standalone value w here  
+//undefined value that is not defined  where variable is set but value is not defined 
+// for example let state = "" this is an undefined value because it is a string value 
+// for null vlaue we can have let state = null this is when somthing has no value for example if any server cannot return value pf somthing you requested 
+
